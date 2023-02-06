77a9fbb6f04aa602c89189f762224f6eef7fac9b
Moin <moinshaikh1592001@gmail.com>
2023-02-06 14:52:41 +0530

    opt-in-form fix

--- main.js
+++ main.js
@@ -422,6 +422,7 @@ function optInForm() {
       cSpecInput.onclick = () => {
         if (error === "speciality") {
           error = "";
+          document.querySelector(".err-msg")?.remove();
         }
       };
       cSpecialityMainDiv.append(cSpecInput);
